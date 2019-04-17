17901fe4e9fcf741fae339b87f1e0e76a4983da8
Sarah Humphreys <sarah.humphreys50@gmail.com>
2019-04-17 21:57:27 +0000

    reverted back

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -23,7 +23,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fill: [500, 250]
+
   # def scale(width, height)
   #   # do something
   # end
